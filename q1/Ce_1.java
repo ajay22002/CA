f0b7b0c879c1295acf3bebf75a539a16efd38d5e
ajay22002 <113581071+ajay22002@users.noreply.github.com>
2023-05-04 16:15:43 +0530

    Add files via upload

---
+++ q1/Ce_1.java
@@ -0,0 +1,61 @@
+package ca.q1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Ce_1
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	ChromeOptions co = new ChromeOptions();
+    	co.addArguments("--remote-allow-origins=*");
+    	WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(co);
+        driver.get("https://www.amazon.in/Samsung-Galaxy-Ultra-Green-Storage/dp/B0BT9CXXXX?ref_=Oct_DLandingS_D_ef5ecf0e_0");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+       String etitle = "";
+        System.out.println(title);
+        if(title.equals(etitle)) {
+        	System.out.println("Title is matching");
+        }
+        else {
+        	System.out.println("Title Mismatch");
+        }
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+        Thread.sleep(5000); 
+        Boolean present = driver.findElement(By.xpath("//*[@id=\"attach-close_sideSheet-link\"]")).isDisplayed();
+        System.out.println("Is item added to cart:" + present);
+        driver.get("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@value=\"Delete\"]")).click();
+        Thread.sleep(2000);
+        String removeText = driver.findElement(By.xpath("//*[@id=\"sc-active-cart\"]/div/div[1]/div/h1")).getText();
+        String extext = "Your Amazon Cart is empty.";
+        if(removeText.equals(extext)) {
+        	System.out.println("Product is removed");
+        }
+        driver.get("https://www.amazon.in/ASUS-15-6-inch-GeForce-Windows-FA506IHRZ-HN111W/dp/B0B5DZTNZQ?ref_=Oct_DLandingS_D_6c1a28b8_61");
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+        Thread.sleep(2000);
+        driver.get("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"a-autoid-0-announce\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"quantity_2\"]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@name=\"proceedToRetailCheckout\"] "));
+        Thread.sleep(1000);
+        String sign = driver.getTitle();
+        System.out.println(sign);
+        String esign = "Amazon Sign In";
+        if(sign.equals(esign)) {
+        	System.out.println("Success");
+        }
+        else {
+        	System.out.println("Failure");
+        }
+    }
+}
